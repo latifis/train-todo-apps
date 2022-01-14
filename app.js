7ee2121e39d1d7045f88219ce837354d0a8f0d3c
Latief Irfansyah <latifirfansyah@gmail.com>
2022-01-14 14:35:44 +0700

    add create and get items

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const Item = require('./models/items');
 const app = express();
 const mongodb = 'mongodb+srv://ckmobile:ckmobile123@cluster0.ljkoi.mongodb.net/item-database?retryWrites=true&w=majority'
 mongoose.connect(mongodb).then(()=>
@@ -9,6 +10,22 @@ mongoose.connect(mongodb).then(()=>
 
 app.set('view engine', 'ejs')
 app.listen(3000);
+
+app.get('/create-item', (req,res)=>{
+    const item = new Item({
+        name:'cocmputer',
+        price:2000
+    })
+    item.save().then(result=>res.send(result)).catch(err=>console.log(err))
+})
+app.get('/get-items', (req,res)=>{
+    
+    Item.find().then(result=>res.send(result)).catch(err=>console.log(err))
+})
+app.get('/get-item', (req,res)=>{
+    
+    Item.findById('61e125d1cba5e7826b8262e3').then(result=>res.send(result)).catch(err=>console.log(err))
+})
 
 app.get('/',(req,res)=>{
     const items = [
