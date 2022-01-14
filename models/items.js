6fcfaad51e01bc8e9f306e19ed5823b5106d0a9b
Latief Irfansyah <latifirfansyah@gmail.com>
2022-01-14 14:23:38 +0700

    add model and schema

---
+++ models/items.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const itemSchema = new Schema({
+    name:{
+        type:String,
+        require:true
+    },
+    price:{
+        type:Number,
+        require:true
+    }
+}, {timestamps:true});
+
+const Item = mongoose.model('Item', itemSchema)
+module.exports = Item;
